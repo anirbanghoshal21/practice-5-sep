b35575d3023bba8b9472cc62fd58330d019d0985
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-05 16:58:28 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    public class BookCategory
+    {
+        private int _catid;
+
+        public int Catid
+        {
+            get { return _catid; }
+            set { _catid = value; }
+        }
+
+        private string _catname;
+
+        public string Catname
+        {
+            get { return _catname; }
+            set { _catname = value; }
+        }
+
+        private string _desc;
+
+        public string Desc
+        {
+            get { return _desc; }
+            set { _desc = value; }
+        }
+    }
+}
