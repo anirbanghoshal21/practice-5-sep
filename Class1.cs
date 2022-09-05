ec2383f6ba11fc0d7363cb29ac7c809e484e2b40
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-05 16:52:36 +0530

    problem 1

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class Book
+    {
+        private int _bookid;
+
+        public int Bookid
+        {
+            get { return _bookid; }
+            set { _bookid = value; }
+        }
+        private string _bname;
+
+        public string BookName
+        {
+            get { return _bname; }
+            set { _bname = value; }
+        }
+
+        private float _price;
+
+        public float Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        private string _author;
+
+        public string author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+        
+
+        }
+    }
+
