2c7e3321532a770eeb9bca13847e85f23eb6151c
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-05 20:44:21 +0530

    prob 8

---
+++ Class3.cs
@@ -0,0 +1,28 @@
+﻿using Class1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class3
+{
+    public class Novel : Book
+    {
+        float _price;
+        public Novel()
+        {
+
+        }
+        public Novel(int bookid)
+        {
+            Console.WriteLine("Executed bookid");
+        }
+        public Novel(int bookid, string bname) : this(bookid)
+        {
+            
+            Console.WriteLine("Executed bookid and bname");
+        }
+
+    }
+}
