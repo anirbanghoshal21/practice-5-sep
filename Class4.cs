ed13c6e920200d8c4cd4091be88a73d5d7883b17
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-05 23:32:02 +0530

    prob 10

---
+++ Class4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class Cart
+    {
+        private int _custid;
+
+        public int Custid
+        {
+            get { return _custid; }
+            set { _custid = value; }
+        }
+
+        private int _bookid;
+
+        public int Bookid
+        {
+            get { return _bookid; }
+            set { _bookid = value; }
+        }
+
+        private float _price;
+
+        public float Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+    }
+}
